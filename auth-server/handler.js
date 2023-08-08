1a61678f1dec35d2c7e8a17a44f382ef7288073b
Matt Gaughan <mattgaugs@gmail.com>
2023-08-08 15:20:12 -0400

    maybe fix 1

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -15,7 +15,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/calendar.events.public.readonly",
 ];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https://mattgaughan2022.github.io/meetApp/"];
+const redirect_uris = ["https://MattGaughan2022.github.io/meetApp/"];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
