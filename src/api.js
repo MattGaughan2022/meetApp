59b8931d78f1ed4b0c7b26efbfb0a3efd0d93688
Matt Gaughan <mattgaugs@gmail.com>
2023-08-09 09:01:56 -0400

    token error

--- src/api.js
+++ src/api.js
@@ -18,7 +18,7 @@ export const getAccessToken = async () =>{
   const accessToken = localStorage.getItem('access_token');
   const tokenCheck = accessToken && (await checkToken(accessToken));
   if (!accessToken || tokenCheck.error){
-    await localStorage.removeItem('accessToken');
+    await localStorage.removeItem('access_Token');
     const searchParams = new URLSearchParams(window.location.search);
     const code = await searchParams.get('code');
     if (!code){
