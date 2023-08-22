329f56376c07aa389b744ad48868c5b06638b998
Matt Gaughan <mattgaugs@gmail.com>
2023-08-22 15:34:47 -0400

    colors

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,8 +48,8 @@ class InfoAlert extends Alert {
   class WarningAlert extends Alert {
     constructor(props) {
       super(props);
-      this.color = 'rgb(0, 255, 255)'; 
-      this.bgColor = 'rgb(220, 255, 255)'; 
+      this.color = 'rgb(0, 0, 0)'; 
+      this.bgColor = 'rgb(255, 255, 0)'; 
     }
   }
 
